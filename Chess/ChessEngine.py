e6b0864c61bf92699e3f15cda5ee55e193611ae1
The24xxx <The24xxx@gmail.com>
2022-09-16 16:18:31 +0200

    Fix typo in introduction to user guide

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -19,12 +19,73 @@ class GameState():
         
         self.whiteToMove = True
         self.moveLog = []
-
+    '''
+    Takes a Move as a parameter and executes it (it will not work for castling, pawn promotion, and en-passant)
+    '''
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
         self.moveLog.append(move) #log the move so we can undo it later
         self.whiteToMove = not self.whiteToMove #swap players
+
+    '''
+    Undo the last move made
+    '''
+
+    def undoMove(self):
+        if len(self.moveLog) != 0: #make sure that there is a move to undo
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove #switch turns back
+
+    '''
+    All moves considering checks
+    '''
+    def getValidMoves(self):
+        return self.getAllPossibleMoves() #for now to not worry about checks
+
+
+    '''
+    All moves without considering checks
+    '''
+    def getAllPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)): #num of rows
+            for c in range(len(self.board[r])): #num of columns in given row
+                turn = self.board[r][c][0] #first character on square (b, w or) - represents color or empty field
+                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    if piece == 'R':
+                        self.getRookMoves(r, c, moves)
+                    if piece == 'N':
+                        self.getKnightMoves(r, c, moves)
+                    if piece == 'B':
+                        self.getBishopMoves(r, c, moves)
+                    if piece == 'Q':
+                        self.getQueenMoves(r, c, moves)
+                    if piece == 'K':
+                        self.getKingMoves(r, c, moves)
+
+    '''
+    Get all the piece moves for the piece located at row, col and add these moves to the list
+    '''
+    def getPawnMoves(self, r, c, moves):
+        pass
+    def getRookMoves(self, r, c, moves):
+        pass
+    def getKnightMoves(self, r, c, moves):
+        pass
+    def getBishopMoves(self, r, c, moves):
+        pass
+    def getQueenMoves(self, r, c, moves):
+        pass
+    def getKingMoves(self, r, c, moves):
+        pass
+    
+
 
 class Move():
     # maps keys to values - translate python coordinations to chess game coordinations
